84705050c49b8c5f3ce26b609ef4704e1f8034ef
Gavin <gavinyahna@brandeis.edu>
2018-10-18 23:42:48 -0400

    add table relationships

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ApplicationRecord
     serialize :requirements, Array
     serialize :subjects, Array
+    has_many :enrollments
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,4 @@
 class Enrollment < ApplicationRecord
+    has_many :users
+    has_many :courses
 end
